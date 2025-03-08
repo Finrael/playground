5d9c110b1d69b85585da852407520ba25ee46a3c
Finrael <finrosdev@gmail.com>
2025-03-08 13:41:09 -0800

    initial pull

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import IndexPage from './pages';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <IndexPage/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
 
 export default App;
- 
