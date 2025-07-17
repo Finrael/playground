e60d2e2717f92db50b5a541d6fd8e82f17c60aa8
Finrael <finrosdev@gmail.com>
2025-07-17 11:56:03 -0700

    adds products controller baseline and base db

--- server.js
+++ server.js
@@ -1,12 +1,47 @@
-let mysql = require('mysql2')
-let con = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"59!Birpu!"
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to  application." });
 });
 
-con.connect(function(err){
-if(err) throw err;
-console.log("Conected!");
-
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port :: ${PORT}.`);
 });
+
+
+
+// const db = require("./src/model/index");
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and re-sync db.");
+// });
+
+// let mysql = require('mysql2')
+// let con = mysql.createConnection({
+//     host:"localhost",
+//     user:"root",
+//     password:"59!Birpu!"
+// });
+
+// con.connect(function(err){
+// if(err) throw err;
+// console.log("Conected!");
+
+// });
---
+++ src/controllers/products.controller.js
@@ -0,0 +1,32 @@
+const db =  require("../model");
+const Product   = db.products;
+const Op   = db.Sequelize.Op;
+
+// Create and save new product
+exports.create = (req, res) => {
+  
+};
+
+//retrieve all products from the database
+exports.findAll = (req, res)=>{
+    
+};
+
+// find a single product with an id
+exports.findOne =(req, res)=>{
+
+};
+
+//update the product by the id in the request
+exports.update =(req, res)=>{
+
+};
+// Delete a products with the specified id in the request
+exports.delete = (req, res) => {
+  
+};
+
+// Delete all products from the database.
+exports.deleteAll = (req, res) => {
+  
+};
